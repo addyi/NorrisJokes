d4ba0f4847e0d47b7790b11b8dd7b78bae06a2d3
Adrian Renner <a.renner+git@eosn.de>
2018-12-13 12:35:23 +0100

    load joke onClick and display joke

--- app/src/main/java/de/eosn/norrisjokes/MainActivity.kt
+++ app/src/main/java/de/eosn/norrisjokes/MainActivity.kt
@@ -2,31 +2,64 @@ package de.eosn.norrisjokes
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.lifecycle.Observer
+import arrow.core.Either
+import com.google.android.material.snackbar.Snackbar
 import de.eosn.norrisjokes.viewmodel.NorrisViewModel
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = SupervisorJob() + Dispatchers.Main
 
     private val tag = MainActivity::class.java.simpleName
 
     private val norrisViewModel: NorrisViewModel by viewModel()
 
+    private lateinit var jokeButton: Button
+    private lateinit var jokeTextView: TextView
+    private lateinit var layout: CoordinatorLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            norrisViewModel.getRandomJoke()
-        }
+        jokeButton = findViewById(R.id.joke_button)
+        jokeTextView = findViewById(R.id.joke_text)
+        layout = findViewById(R.id.main_activity)
+
+        jokeButton.setOnClickListener { launch { loadJoke() } }
+
+        launch { loadJoke() }
 
         norrisViewModel.chuckNorrisJokes.observe(this, Observer {
             for (chuckNorrisJoke in it) {
                 Log.d(tag, chuckNorrisJoke.toString())
             }
         })
+    }
+
+    private suspend fun loadJoke() {
+        val eitherJokeOrException = norrisViewModel.getRandomJoke()
+
+        when (eitherJokeOrException) {
+            is Either.Left -> jokeTextView.text = eitherJokeOrException.a.joke
+            is Either.Right -> Snackbar.make(
+                layout,
+                "Something went wrong",
+                Snackbar.LENGTH_LONG
+            ).show()
+
+        }
     }
 }
